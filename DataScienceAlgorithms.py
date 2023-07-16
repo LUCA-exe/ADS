c28601a5bad565130936edc469e6f6b942ca80cf
LUCA-exe <39984220+LUCA-exe@users.noreply.github.com>
2023-07-16 15:10:18 +0200

    Updated 'KNearestNeighbors' function

--- DataScienceAlgorithms.py
+++ DataScienceAlgorithms.py
@@ -1,6 +1,22 @@
 import numpy as np
 import pandas as pd
-from collections import Counter
+from collections import Counter, defaultdict
+from math import floor
+
+# Utils function
+def check_variable_value(variable, value_list):
+    """
+    :param variable: The variable to check
+    :param value_list: List of values allowed
+    :return: True if the variable is correctly set
+    """
+    if not isinstance(variable, str):
+        raise TypeError("This parameter should be a string.")
+
+    if variable not in value_list:
+        raise ValueError("Variable value not found in the list.")
+
+    return True
 
 class DatasetWrapper():
 
@@ -41,68 +57,120 @@ class DatasetWrapper():
 
     def get_split_data(self, split_percentage=0.8):
         """ Function to retrieve the Train, Test split using the 'mask'
-        :return: Tuple of numpy vectors
+        :param split_percentage: Split of dataset to be used as training set.
+        :return: Numpy vectors (X/y split) returned as tuple
         """
-        X = self.df.values[:, :4].astype(float)  # all rows (:), columns 0 -> 3 (:4)
+        X = self.df.values[:, :4].astype(float)
         y = self.df.values[:, 4]
-        mask = np.array([True] * 120 + [False] * 30)
+        train_num = floor(X.shape[0] * 0.8) # Apply the percentage split
+        mask = np.array([True] * train_num + [False] * (X.shape[0] - train_num))
+        np.random.shuffle(mask)
 
-        return self.raw_data
+        # Retrieve the splits
+        X_train, X_test = X[mask], X[~mask]
+        y_train, y_test = y[mask], y[~mask]
 
-# Optimized implementation of 'KNearestNeighbors' algorithm using numpy
+        return (X_train, X_test, y_train, y_test)
+
+# Implementation of 'KNearestNeighbors' algorithm using numpy
 class KNearestNeighbors():
 
-    def __init__(self, k, distance_metric="euclidean"):
+    def __init__(self, k, distance_metric="euclidean", weights="uniform"):
         """ Custom class to implement the k-NearestNeighbors classification algorithm through the Numpy package
         :param k:
         :param distance_metric:
         """
         self.k = k
         self.distance_metric = distance_metric
+        self.weight_mode = weights
+        self.allowed_weight_modes = ["uniform", "weighted"]
+        self.allowed_distance_metrics = ["euclidean", "cosine"]
+        # Checking integrity of the paramters
+        check_variable_value(self.distance_metric, self.allowed_distance_metrics)
+        check_variable_value(self.weight_mode, self.allowed_weight_modes)
 
     def fit(self, X_train, y_train):
+        """ Store and prepare the dataset to predict unlabeled data points
+        :param X_train: Training features of the dataset
+        :param y_train: Set of labels for each data point
+        :return: None
+        """
         self.X_train = X_train
         self.y_train = y_train
-
-        # Prepare the broadcast
+        # Prepare the broadcast for differents similarities fuction
         self.X_train_reshaped = np.expand_dims(self.X_train, 1) # Data prepared for the "eucldean distance" distance implementation
         self.X_train_norm = ((self.X_train ** 2).sum(axis=1) ** .5).reshape(-1, 1)
 
+        return None
+
     def euclidean(self, X_test):
+        """ Compute the euclidean distance
+        :param X_test: Features from the unlabeled data points
+        :return: Matrix of the distance along the all dimension for each pair of train and test data point
+        """
         X_diff = self.X_train_reshaped - X_test
         dist_matrix = ((X_diff ** 2).sum(axis=2)) ** .5
         return dist_matrix
 
+    def cosine(self, X_test):
+        """ Compute the cosine distance
+        :param X_test: Features from the unlabeled data points
+        :return: Matrix of the distance along the all dimension for each pair of train and test data point
+        """
+        X_test_norm = ((X_test ** 2).sum(axis=1) ** .5).T
+        dot_prods = self.X_train @ X_test.T # Matrix multiplication
+        dist_matrix = 1 - abs(dot_prods / self.X_train_norm.reshape(-1, 1) / X_test_norm)
+        return dist_matrix
+
     def majority_voting(self, votes):
         """ Naive implementation to assign a label
-        :param votes: Set of labels (string object in this case)
+        :param votes: Set of labels
         :return: More common object
         """
         count = Counter(votes)
         return count.most_common(1)[0][0]
 
+    def weighted_majority_voting(self, votes, weights):
+        """ Weighted voting system based on the distance
+        :param votes: Set of labels
+        :param weights: Weights of the labels to consider
+        :return: The label based on distances from the unlabeled point
+        """
+        count = defaultdict(lambda: 0)
+        for vote, weight in zip(votes, weights):
+            count[vote] += weight
+        return max(count.items(), key=lambda x: x[1])[0]  # return the max value (use a custom key extractor)
+
     def predict(self, X_test):
         """ Use the train set to label the test data points
 
         :param X_test: Data points to label
-        :return: Predicted labels
+        :return: Predicted labels as an array of values
         """
         if self.distance_metric == "euclidean":
             dist_matrix = self.euclidean(X_test)
+        elif self.distance_metric == "cosine":
+            dist_matrix = self.cosine(X_test)
+
         # For every data point, select the smallest data point in the original train_set (considering the 'idx')
-        knn = dist_matrix.argsort(axis=0)[:self.k, :].T # Transpose the matrix for the final labeling
-        y_pred = np.array([self.majority_voting(self.y_train[knn][i]) for i in range(len(self.y_train[knn]))])
+        knn = dist_matrix.argsort(axis=0)[:self.k, :].T # Transpose the matrix for the final labeling (final shape of the idx is Nxk)
+
+        if self.weight_mode == "uniform":
+            y_pred = np.array([self.majority_voting(self.y_train[knn][i]) for i in range(len(self.y_train[knn]))])
+        elif self.weight_mode == "weighted":
+            weights = 1 / (np.take_along_axis(dist_matrix, knn.T, 0) + 1e-5) # Add a small value to counter the "0" denominator
+            y_pred = np.array([self.weighted_majority_voting(self.y_train[knn][i], weights[:, i]) for i in range(len(self.y_train[knn]))])
         return y_pred
 
-
-
-# Implement the custom class
+# Implement the custom class of the classificator with a toy dataset
 def main():
     df = DatasetWrapper("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
     df.preliminary_exploration()
     knn_model = KNearestNeighbors(3, "euclidean")
+    X_train, X_test, y_train, y_test = df.get_split_data()
     knn_model.fit(X_train, y_train)
     y_pred = knn_model.predict(X_test)
+    print(y_pred)
 
 if __name__ == '__main__':
     main()
